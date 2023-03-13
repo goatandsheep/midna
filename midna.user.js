06a97ed3e0ffb5e653dbf219303c69a59dc06372
Kemal Ahmed <goatandsheep@users.noreply.github.com>
2023-03-13 09:11:28 -0400

    Create midna.user.js

---
+++ midna.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Midna
+// @version      0.1
+// @description  Browser companion to take control of breaks
+// @author       Kemal Ahmed <goatandsheep@gmail.com>
+// @match        *
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+console.log('searching');
+    // Your code here...
+})();
