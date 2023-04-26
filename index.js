897a4daa0a9328393c188ed790aa1eafcdbfc642
Kemal Ahmed <goatandsheep@gmail.com>
2023-04-25 22:15:42 -0400

    this should work now

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const i18n = {
         'isThisWork': 'Are you here for work? If ya, say OK, otherwise say Cancel.',
         'doNeedTimer': 'Can I set a timer for you? If yes, how many minutes? If not, press Cancel.',
         'considerAlt': 'Also I\'m worried you didn\'t consider taking an off-screen break.',
-        'considerAvoidance': 'I know you\'re working hard, so if this is just a tough work issue, you may benefit from moving on to some other work, taking a step back to look at the bigger picture, asking for help, or pushing through for 5 more minutes. Are you overdue for a break? Maybe you should take this break. If you still want this break, can I set a timer? If so, tell me how many minutes.',
+        'considerAvoidance': 'I know you\'re working hard, so if this is just a tough work issue, consider moving on to some other work, taking a step back to look at the bigger picture, asking for help, or pushing through for 5 more minutes. Also consider if you\'re overdue for a break. Maybe you should take this break. Will you cancel this break?',
         'factWorkTimers': 'I think you should because this page is engineered to lead you to content relevant to work, but is still avoidance. Also you are already addicted to working too much and going without breaks is bad for your productivity and health.',
         'enjoyBreak': 'Enjoy your break!',
         'enjoyWork': 'Good luck on your work!',
@@ -32,34 +32,46 @@ if (timer && timer > currDate.getTime()) {
 const isReflex = window.alert(`${midnaText}: ${i18n[lang]['flaggedSiteWarning']} ${i18n[lang]['catchReminder']} ${i18n[lang]['confirmContinue']}`)
 
 if (window.confirm(`${midnaText}: ${i18n[lang]['isThisWork']}`)) {
+    // this is work
+    timerLoop('factWorkTimers')
+} else if(window.confirm(`${midnaText}: ${i18n[lang]['isThisPlanned']} ${i18n[lang]['considerAlt']}`)) {
+    // set timer and enjoy planned break
+    timerLoop('enjoyBreak')
+} else if(window.confirm(`${midnaText}: ${i18n[lang]['considerAvoidance']}`)) {
+    // if you are here to avoid work, it suggests alternatives or you go back
+    history.back()
+} else {
+    // set unplanned break timer
+    timerLoop('enjoyBreak')
+}
+
+/**
+ *
+ * @param {String} messageKey prompt
+ */
+function timerLoop(messageKey) {
     let invalid = true
     let timerTime = ''
     while(invalid) {
         let showError = ''
         if (timerTime) {
-            // TODO: show error message
+            // show error message
             showError += ('\n' + i18n[lang]['errorNumberExpected'])
         }
-        timerTime = window.prompt(`${midnaText}: ${i18n[lang]['doNeedTimer']} ${i18n[lang]['factWorkTimers']}${showError}`)
+        timerTime = window.prompt(`${midnaText}: ${i18n[lang]['doNeedTimer']} ${i18n[lang][messageKey]}${showError}`)
         if (!timerTime) {
+            // you don't want a timer
             invalid = false
         }
         if (timerTime && !isNaN(parseInt(timerTime))) {
+            // create timer
             createTimer(timerTime)
             invalid = false
         }
     }
-} else if(window.confirm(`${midnaText}: ${i18n[lang]['isThisPlanned']} ${i18n[lang]['considerAlt']}`)) {
-    // TODO: set timer and enjoy
-    window.prompt(`${midnaText}: ${i18n[lang]['doNeedTimer']} ${i18n[lang]['enjoyBreak']}`)
-} else {
-    // if you are here to avoid work, suggest alertatives
-    window.prompt(`${midnaText}: ${i18n[lang]['considerAvoidance']} ${i18n[lang]['doNeedTimer']}`)
-    // TODO: set timer?
-    // TODO: save timer?
 }
+
 /**
- *
  * @param {Number} time in minutes
  */
 function createTimer(time) {
@@ -82,14 +94,13 @@ function restartTimer(timestamp) {
     }
 }
 
-// clearInterval
-
 /**
  * Notify the user the end of the timer, give a message, ask if they want another timer
  */
 function timerEnd() {
-    // TODO: how are you? would you like another timer? you probably shouldn't as you are having diminishing returns
-    // alternatively if you want to continue resting, consider changing your form of break such as exercise, eating, napping, etc.
+    if (window.confirm(`${midnaText}: You asked me to set a timer until now so you can maintain control of your schedule. I hope your break was relaxing! You probably shouldn\'t continue your break as your break returns are diminished. If you want to continue resting, consider changing your form of break such as exercise, eating, napping, etc. Are you going to continue your break?`)) {
+        timerLoop('considerAlt')
+    }
 }
 
 // abstracted so this script can be used as an extension or a userscript or something else
